e01a51852eb13a53b2da7d86625bd1d546277c2b
Shai Weinstein <s@s.com>
2026-02-19 23:11:19 -0500

    maybe fix acc issue when we lose lead car display from cluster, and then radar

--- opendbc/car/volkswagen/mlbcan.py
+++ opendbc/car/volkswagen/mlbcan.py
@@ -135,7 +135,7 @@ def create_acc_hud_control(packer, bus, acc_hud_status, set_speed, lead_distance
     "ACC_Status_Anzeige": acc_hud_status,
     "ACC_Wunschgeschw_02": set_speed if set_speed < 250 else 327.36,
     "ACC_Gesetzte_Zeitluecke": zeitluecke,  # Mirror stock radar's ZL from ext bus (responds to DIST button)
-    "ACC_Display_Prio": 3,
+    "ACC_Display_Prio": 2 if lead_object else 3,
     "ACC_Abstandsindex": lead_distance,
     "ACC_Relevantes_Objekt": lead_object,
   }
